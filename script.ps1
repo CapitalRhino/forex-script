640139c27c8675fdff3967b1d482ae9b5b3c7d0c
Dimitar Byalkov <CapitalRhino@users.noreply.github.com>
2021-07-28 14:08:57 +0300

    WebException handling and quotes remover

--- script.ps1
+++ script.ps1
@@ -4,7 +4,7 @@
 #>
 
 <# parameter definition #>
-param ($inputFile, $outputFile, $dayCount)
+param ($inputFile, $outputFile, $downloadDirectory, $dayCount)
 
 <# paramater validation #>
 if (!(Test-Path -Path "inputfile.csv" -PathType Leaf) -and ($inputFile -eq $null)) {
@@ -16,14 +16,30 @@ if ($inputFile -eq $null) {
 if ($outputFile -eq $null) {
     $outputFile = "export.csv"
 }
+if ($downloadDirectory -eq $null) {
+    $downloadDirectory = "download"
+}
 if ($dayCount -eq $null) {
     $dayCount = 1
 }
+
+$execDir = Split-Path $MyInvocation.MyCommand.Path
 
 <# clear existing $outputFile #>
 if ((Test-Path $outputFile) -eq $true) {
   Remove-Item $outputFile
   Write-Host "INFO: Cleared existing output file."
+}
+<# delete download directory if found #>
+if ((Test-Path $downloadDirectory) -eq $true) {
+  Remove-Item $downloadDirectory -Recurse
+  Write-Host "INFO: Cleared existing download directory."
+}
+
+<# create download directory if not found#>
+if (!(Test-Path -Path $downloadDirectory -PathType Leaf)) {
+    [void](New-Item -Path $execDir -Name $downloadDirectory -ItemType Directory)
+    Write-Host "INFO: Created download directory."
 }
 
 <# UNIX time converter #>
@@ -51,25 +67,58 @@ ForEach ($forex in $data) {
     [void]$url.Append('&interval=1d&events=history&includeAdjustedClose=true')
 
     $downloadURL = [uri]($url.ToString())
-    $localURL = "$(Split-Path $MyInvocation.MyCommand.Path)\$($downloadURL.segments[-1])"
+    $localURL = "$execDir\$downloadDirectory\$($downloadURL.segments[-1])"
 
     <# API connection #>
-    $WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile($downloadURL, $localURL)
-    Write-Host "INFO: $symbol downloaded."
+    try {
+        $WebClient = New-Object System.Net.WebClient
+        $WebClient.DownloadFile($downloadURL, $localURL)
+        Write-Host "INFO: $symbol downloaded."
 
-    <# import downloaded CSV file #>
-    $apiResponse = Import-Csv $localURL
+        <# import downloaded CSV file #>
+        $apiResponse = Import-Csv $localURL
 
-    <# add one-by-one output lines #>
-    ForEach ($row in $apiResponse) {
-        $objResults = New-Object PSObject -Property ([ordered]@{
-            forex_id = $forexID;
-            symbol   = $symbol;
-            date     = $($row.Date);
-            rate     = $($row.Close);
-        })
-        $objResults | Export-Csv -Append -NoTypeInformation -Path $outputFile
+        <# add one-by-one output lines #>
+        ForEach ($row in $apiResponse) {
+            $objResults = New-Object PSObject -Property ([ordered]@{
+                forex_id = $forexID;
+                symbol   = $symbol;
+                date     = $($row.Date);
+                rate     = $($row.Close);
+            })
+            $objResults | Export-Csv -Append -NoTypeInformation -Path $outputFile 
+        }
+        Write-Host "INFO: $symbol complete."
     }
-    Write-Host "INFO: $symbol complete."
+    catch [System.Net.WebException]  {
+        $statusCodeValue = $_.Exception.Response.StatusCode.value__
+        switch ($statusCodeValue) {
+            400 {
+                Write-Warning -Message "$symbol HTTP 400 Bad Request"
+            }
+            401 {
+                Write-Warning -Message "$symbol HTTP 401 Unauthorized."
+            }
+            403 {
+                Write-Warning -Message "$symbol HTTP 403 Forbidden"
+            }
+            404 {
+                Write-Warning -Message "$symbol HTTP 404 Not found. Invalid symbol."
+            }
+            500 {
+                Write-Warning -Message "$symbol HTTP 500 Internal Server Error"
+            }
+            Default {
+                Write-Warning "$symbol download failed!"
+            }
+        }
+    }
+    catch {
+        Write-Warning "Unknown error while proccessing $symbol!"
+    }
+
 }
+
+<# Remove double quotes from data #>
+(Get-Content $outputFile) | Foreach-Object {$_ -replace '"', ''} | Out-File $outputFile
+Write-Host "INFO: Removed quotes from data."
